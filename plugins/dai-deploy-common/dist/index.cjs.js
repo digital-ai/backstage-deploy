22985c818a4481686b675f204f07e814250eaddd
muthuraj-mariappan <70747268+muthuraj-mariappan@users.noreply.github.com>
2024-02-05 13:41:22 +0530

    remove output files

--- plugins/dai-deploy-common/dist/index.cjs.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
-const COMMON_CONSTANT = 1;
-
-exports.COMMON_CONSTANT = COMMON_CONSTANT;
-//# sourceMappingURL=index.cjs.js.map
--- plugins/dai-deploy-common/dist/index.esm.js
+++
@@ -1,4 +0,0 @@
-const COMMON_CONSTANT = 1;
-
-export { COMMON_CONSTANT };
-//# sourceMappingURL=index.esm.js.map
