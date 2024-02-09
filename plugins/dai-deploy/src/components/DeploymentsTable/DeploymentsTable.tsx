3b05834a93da7599c9d8636e42ca4d6896d34ea0
muthuraj-mariappan <70747268+muthuraj-mariappan@users.noreply.github.com>
2024-02-09 15:58:24 +0530

    S-106199 - add link for taskId

--- plugins/dai-deploy/src/components/DeploymentsTable/DeploymentsTable.tsx
+++ plugins/dai-deploy/src/components/DeploymentsTable/DeploymentsTable.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Table, TableColumn } from '@backstage/core-components';
+import LaunchIcon from '@material-ui/icons/Launch';
+import { LinkButton, Table, TableColumn } from '@backstage/core-components';
 
 const columns: TableColumn[] = [
     {
@@ -45,6 +46,16 @@ const columns: TableColumn[] = [
       width: 'auto',
       type: 'datetime',
     },
+    {
+      title: 'View',
+      field: 'taskId',
+      width: 'auto',
+      render: (row: Partial<any>) => (
+        <LinkButton to={`http://localhost:4516/#/reports/deployments?taskId=${row.taskId}`}>
+          <LaunchIcon />
+        </LinkButton>
+      ),
+    },
   ];
   
   export const DeploymentsTable = () => {
@@ -58,7 +69,8 @@ const columns: TableColumn[] = [
             "state": "Done",
             "scheduledDate": "",
             "startDate": "2024-02-01T08:43:44.480+0000",
-            "endDate": "2024-02-01T08:46:16.915+0000"
+            "endDate": "2024-02-01T08:46:16.915+0000",
+            "taskId": "fe62430f-6ce6-41fc-a5e0-191ad48fa8c1"
         },
         {
           "package": "D-32171/2.0",
@@ -68,7 +80,8 @@ const columns: TableColumn[] = [
           "state": "Done",
           "scheduledDate": "",
           "startDate": "2024-01-31T06:43:44.480+0000",
-          "endDate": "2024-01-31T08:46:16.915+0000"
+          "endDate": "2024-01-31T08:46:16.915+0000",
+          "taskId": "1b1cfecb-3e81-499d-b415-dbd11dfe248c"
       }
     ]
     return (<Table 
