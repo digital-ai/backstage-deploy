27219b1f0e10f2235cc7d22919a925063166f992
Ishwarya Surendrababu <sishwarya@digital.ai>
2024-02-23 09:42:06 +0530

    Missing annotation tag added for deploycicd

--- plugins/dai-deploy/src/components/DaiDeployEntityDeploymentsContent/DaiDeployEntityDeploymentsContent.tsx
+++ plugins/dai-deploy/src/components/DaiDeployEntityDeploymentsContent/DaiDeployEntityDeploymentsContent.tsx
@@ -5,23 +5,33 @@ import {DeploymentsTable} from "../DeploymentsTable";
 // @ts-ignore
 import deployLogo from '../../assets/deployLogo.svg';
 import { CardTab, TabbedCard } from '@backstage/core-components';
+import {MissingAnnotationEmptyState, useEntity} from "@backstage/plugin-catalog-react";
+import {DAI_DEPLOY_CI_ID_ANNOTATION} from "@digital-ai/plugin-dai-deploy-common";
+import {isCiCdAvailable} from "../isCiCdAvailable";
 
 export const DaiDeployEntityDeploymentsContent = () => {
-    return (
-        <TabbedCard
-            title={
-                <Box display="flex" alignItems="center">
-                <img src={deployLogo} alt="Deploy logo" height="30px" />
-                <Box mr={2} />
-                <Typography variant="h6">Digital ai Deploy</Typography>
-                </Box>}
-        >
-            <CardTab label="Status">
-                <DeploymentsTable />
-            </CardTab>
-            <CardTab label="Reports">
-                <div>Some content 2</div>
-            </CardTab>
-        </TabbedCard>
-    );
+    // @ts-ignore
+    const { entity } = useEntity();
+    if(isCiCdAvailable(entity)){
+        return (
+            <TabbedCard
+                title={
+                    <Box display="flex" alignItems="center">
+                        <img src={deployLogo} alt="Deploy logo" height="30px" />
+                        <Box mr={2} />
+                        <Typography variant="h5">Digital ai Deploy1</Typography>
+                    </Box>}
+            >
+                <CardTab label="Deployment Status">
+                    <DeploymentsTable api={"current"} />
+                </CardTab>
+                <CardTab label="Deployment Reports">
+                    <DeploymentsTable api={"report"}/>
+                </CardTab>
+            </TabbedCard>
+        );
+    } else {
+       return (<MissingAnnotationEmptyState annotation={DAI_DEPLOY_CI_ID_ANNOTATION} />);
+    }
+
 };
