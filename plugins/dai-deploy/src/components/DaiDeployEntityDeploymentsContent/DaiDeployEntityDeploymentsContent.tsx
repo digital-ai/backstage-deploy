16f9dd76f93a57178dc675195670332cbe221588
Ishwarya Surendrababu <sishwarya@digital.ai>
2024-02-23 13:12:35 +0530

    TK-88760 paper tag added for the deployemntentity

--- plugins/dai-deploy/src/components/DaiDeployEntityDeploymentsContent/DaiDeployEntityDeploymentsContent.tsx
+++ plugins/dai-deploy/src/components/DaiDeployEntityDeploymentsContent/DaiDeployEntityDeploymentsContent.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 import {DeploymentsTable} from "../DeploymentsTable";
 // @ts-ignore
 import deployLogo from '../../assets/deployLogo.svg';
-import { CardTab, TabbedCard } from '@backstage/core-components';
+import {CardTab, TabbedCard} from '@backstage/core-components';
 import {MissingAnnotationEmptyState, useEntity} from "@backstage/plugin-catalog-react";
 import {DAI_DEPLOY_CI_ID_ANNOTATION} from "@digital-ai/plugin-dai-deploy-common";
 import {isCiCdAvailable} from "../isCiCdAvailable";
+import {Box, Paper} from "@material-ui/core";
+import Typography from "@mui/material/Typography";
 
 export const DaiDeployEntityDeploymentsContent = () => {
-    // @ts-ignore
     const { entity } = useEntity();
     if(isCiCdAvailable(entity)){
         return (
+            <Paper elevation={1} >
+            <div>
+                <Box display="inline-flex" alignItems="center" paddingTop={3} paddingLeft={2} paddingBottom={2}>
+                    <img src={deployLogo} alt="Deploy logo" height="30px"/>
+                    <Box mr={2}/>
+                    <Typography variant="h5">Digital ai Deploy</Typography>
+                </Box>
             <TabbedCard
-                title={
-                    <Box display="flex" alignItems="center">
-                        <img src={deployLogo} alt="Deploy logo" height="30px" />
-                        <Box mr={2} />
-                        <Typography variant="h5">Digital ai Deploy1</Typography>
-                    </Box>}
-            >
+                title="">
                 <CardTab label="Deployment Status">
                     <DeploymentsTable api={"current"} />
                 </CardTab>
@@ -29,7 +29,10 @@ export const DaiDeployEntityDeploymentsContent = () => {
                     <DeploymentsTable api={"report"}/>
                 </CardTab>
             </TabbedCard>
+            </div>
+            </Paper>
         );
+
     } else {
        return (<MissingAnnotationEmptyState annotation={DAI_DEPLOY_CI_ID_ANNOTATION} />);
     }
